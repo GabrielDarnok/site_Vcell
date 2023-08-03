fd192dd363c2eefa715f4b83775e2bb795d0ad1c
Gabriel Darnok <silva0349@gmail.com>
2023-08-02 21:09:25 -0300

    Inserção do banco de dados no codigo.

--- Teste_site/app/Http/Controllers/Eventcontroller.php
+++ Teste_site/app/Http/Controllers/Eventcontroller.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use App\Models\Products;
 
 class Eventcontroller extends Controller
 {
-    public function index2 (){
-        $nome = "Gabriel";
-        $array = [1,2,3,4,5];   
-        return view('welcome', ['nome' => "Gabriel"], ['array' => $array]);
+    public function index2 () {
+
+        $Product = Products::all();
+
+        return view('welcome', ['Product' => $Product]);
     }
 
     public function create(){
@@ -23,22 +25,27 @@ class Eventcontroller extends Controller
     }
 
     public function index (){
+
         return view('index');
     }
 
     public function product (){
+
         return view('product');
     }
 
     public function store (){
+
         return view('store');
     }
 
     public function checkout (){
+
         return view('checkout');
     }
 
     public function trace (){
+
         return view('trace');
     }
 }
