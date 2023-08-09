ccca1c9595901e0d2345791c4b7e6e9e01f33a84
Gabriel Darnok <silva0349@gmail.com>
2023-08-09 17:24:07 -0300

    Comentando os controllers

--- Teste_site/app/Http/Controllers/RedirectController.php
+++ Teste_site/app/Http/Controllers/RedirectController.php
@@ -8,29 +8,30 @@ use App\Models\Products;
 
 class RedirectController extends Controller
 {
+    #Retornando a pagina Index ao usuario
     public function index () {
         return view('index');
     }
-
-    public function create(){
-        return view('produto.create');
-    }
     
+    #Retornando a pagina Checkout ao usuario
     public function checkout (){
 
         return view('checkout');
     }
 
+    #Retornando a pagina trace ao usuario
     public function trace (){
 
         return view('trace');
     }
 
+    #Retornando a pagina Cadastrar ao usuario
     public function cadastrar(){
 
         return view('cadastrar');
     }
 
+    #Retornando a pagina store_product ao usuario
     public function store_product(){
 
         return view('loja.store_product');
